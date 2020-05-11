814c76a0338db922a75b0a7b7cdb3e291e63d039
JonnyKong <jonnykong1996@gmail.com>
2020-05-10 17:19:32 -0700

    utils: pub-sub bug fix

--- ndn_python_repo/utils/__init__.py
+++ ndn_python_repo/utils/__init__.py
@@ -1,1 +1,2 @@
 from .concurrent_fetcher import concurrent_fetcher
+from .pubsub import PubSub
--- ndn_python_repo/utils/pubsub.py
+++ ndn_python_repo/utils/pubsub.py
@@ -1,8 +1,11 @@
 """
     Pub-sub API.
 
-    This pub-sub library provides best-effort, at-most-once delivery guarantee. If there are
-    multiple subscribers, the nearest subscriber will be notified in an any-cast style.
+    This pub-sub library provides best-effort, at-most-once delivery guarantee.
+    if there are no subscribers reachable when a message is published, this message will not be
+    re-transmitted.
+    If there are multiple subscribers reachable, the nearest subscriber will be notified of the
+    published message in an any-cast style.
 
     @Author jonnykong@cs.ucla.edu
     @Date   2020-05-08
@@ -12,114 +15,184 @@ import asyncio as aio
 import logging
 from ndn.app import NDNApp
 from ndn.encoding import TlvModel, NameField, UintField
-from ndn.encoding import Name, NonStrictName, Component
+from ndn.encoding import Name, NonStrictName, Component, InterestParam
+from ndn.name_tree import NameTrie
 from ndn.types import InterestNack, InterestTimeout
 from ndn.utils import gen_nonce
 
 
-class NotifyAppParam(TlvModel):
-    """
-    Serialize application parameters for PubSub notify interest.
-    """
-    publisher_prefix = NameField()
-    nonce = UintField(128)
-    publisher_fwd_hint = NameField()
-
-
 class PubSub(object):
     """
-    Listens on `/prefix/notify` and `/prefix/msg`.
+    Best-effort, at-most-once publish-subscribe.
+    TODO: support msg larger than MTU
     """
+
+    class NotifyAppParam(TlvModel):
+        """
+        Used to serialize application parameters for PubSub notify interest.
+        """
+        publisher_prefix = NameField()
+        nonce = UintField(128)
+        publisher_fwd_hint = NameField()
 
     def __init__(self, app: NDNApp, prefix: NonStrictName, forwarding_hint: NonStrictName=None):
         """
         :param app: NDNApp.
         :param prefix: NonStrictName.
+        :param forwarding_hint: NonStrictName. When working as publisher, if `prefix` is not
+        reachable, the subscriber can use `forwarding_hint` to reach the publisher.
         """
         self.app = app
         self.prefix = prefix
         self.forwarding_hint = forwarding_hint
         self.nonce_to_msg= dict()
-        self.topic_to_cb = dict()
+        self.topic_to_cb = NameTrie()
 
     async def wait_for_ready(self):
         """
         Need to be called to wait for pub-sub to be ready.
         """
-        await self.app.register(self.prefix + ['notify'], self._on_notify_interest)
         await self.app.register(self.prefix + ['msg'], self._on_msg_interest)
 
     def publish(self, topic: NonStrictName, msg: bytes):
         """
-        Synchronous wrapper for ``_publish_helper()``.
+        Publish `msg` to `topic`.
+
+        :param topic: NonStrictName. The topic to publish `msg` to.
+        :param msg: bytes. The message to publish. The pub-sub API does not make any assumptions on
+        the format of this message.
         """
         aio.ensure_future(self._publish_helper(topic, msg))
 
     def subscribe(self, topic: NonStrictName, cb: callable):
-        logging.info(f'subscribing to topic: {Name.to_str(topic)}')
-        topic = Name.normalize(topic)
-        self.topic_to_cb[topic] = cb
+        """
+        Subscribe to `topic` with `cb`.
+
+        :param topic: NonStrictName. The topic to subscribe to.
+        :param cb: callable. A callback that will be called when a message under `topic` is
+        received. This function takes one `bytes` argument.
+        """
+        aio.ensure_future(self._subscribe_helper(topic, cb))
 
     def unsubscribe(self, topic: NonStrictName):
+        """
+        Unsubscribe from `topic`.
+
+        :param topic: NonStrictName. The topic to unsubscribe from.
+        """
         logging.info(f'unsubscribing topic: {Name.to_str(topic)}')
         topic = Name.normalize(topic)
         del self.topic_to_cb[topic]
 
-    async def _publish_helper(self, topic: NonStrictName, msg: bytes):
-        nonce = gen_nonce()
 
-        # wrap ``msg`` into a data packet
-        # TODO: erase soft state
-        data_name = self.prefix + [Component.from_number(nonce)]
+
+    async def _publish_helper(self, topic: NonStrictName, msg: bytes):
+        """
+        Async helper for `subscribe()`.
+        """
+        logging.info(f'publishing a message to topic: {Name.to_str(topic)}')
+        # generate a nonce for each message
+        nonce = gen_nonce()
         self.nonce_to_msg[nonce] = msg
 
         # prepare notify interest
+        # int_name = topic + [Component.from_str('notify')]
         int_name = topic + ['notify']
-        app_param = NotifyAppParam()
+        app_param = PubSub.NotifyAppParam()
         app_param.publisher_prefix = self.prefix
         app_param.nonce = nonce
         if self.forwarding_hint:
             app_param.forwarding_hint = self.forwarding_hint
 
+        aio.ensure_future(self._erase_state_after(nonce, 5))
+
         # express notify interest
         try:
+            logging.debug(f'sending notify interest: {Name.to_str(int_name)}')
             data_name, meta_info, content = await self.app.express_interest(
                 int_name, app_param.encode(), must_be_fresh=False, can_be_prefix=False)
         except InterestNack as e:
-            logging.warning(f'Nacked with reason: {e.reason}') 
+            logging.debug(f'Nacked with reason: {e.reason}')
             return
         except InterestTimeout:
-            logging.warning(f'Timeout')
+            logging.debug(f'Timeout')
             return
-        logging.info(f'received notify response: {data_name}')
-        logging.info('will erase soft state')
 
-    async def _on_notify_interest(self, int_name, int_param, app_param):
-        logging.info(f'received notify interest: {Name.to_str(int_name)}')
-        topic = int_name[:-1]
-        assert topic in self.topic_to_cb
-        assert Component.to_str(int_name[-1]) == 'notify'
+        # if receiving notify response, the subscriber has finished fetching msg
+        logging.debug(f'received notify response: {data_name}')
+        await self._erase_state_after(nonce, 0)
+
+    async def _subscribe_helper(self, topic: NonStrictName, cb: callable):
+        """
+        Async helper for `subscribe()`.
+        """
+        logging.info(f'subscribing to topic: {Name.to_str(topic)}')
+        topic = Name.normalize(topic)
+        self.topic_to_cb[topic] = cb
+        await self.app.register(topic + ['notify'], self._on_notify_interest)
+
+    def _on_notify_interest(self, int_name, int_param, app_param):
+        aio.ensure_future(self._process_notify_interest(int_name, int_param, app_param))
+
+    async def _process_notify_interest(self, int_name, int_param, app_param):
+        """
+        Async helper for `_on_notify_interest()`.
+        """
+        logging.debug(f'received notify interest: {Name.to_str(int_name)}')
+        topic = int_name[:-2]   # remove digest and `notify`
 
         # parse notify interest
-        app_param = NotifyAppParam.parse(app_param)
+        app_param = PubSub.NotifyAppParam.parse(app_param)
         publisher_prefix = app_param.publisher_prefix
         nonce = app_param.nonce
         publisher_fwd_hint = app_param.publisher_fwd_hint
+        int_param = InterestParam()
+        if publisher_fwd_hint:
+            int_param.forwarding_hint = publisher_fwd_hint
 
         # send msg interest
-        int_name = publisher_fwd_hint + [nonce]
-        data_name, meta_info, msg = await self.app.express_interest(int_name)
+        msg_int_name = publisher_prefix + ['msg', str(nonce)]
+        try:
+            logging.debug(f'sending msg interest: {Name.to_str(msg_int_name)}')
+            data_name, meta_info, msg = await self.app.express_interest(
+                msg_int_name, int_param=int_param)
+        except InterestNack as e:
+            logging.debug(f'Nacked with reason: {e.reason}')
+            return
+        except InterestTimeout:
+            logging.debug(f'Timeout')
+            return
 
         # pass msg to application
-        self.topic_to_cb[topic](msg)
+        logging.info(f'received subscribed msg: {Name.to_str(msg_int_name)}')
+        self.topic_to_cb[topic](bytes(msg))
 
-    async def _on_msg_interest(self, int_name, int_param, app_param):
-        logging.info(f'received msg interest: {Name.to_str(int_name)}')
-        nonce = Component.to_number(int_name[-1])
-        
+        # acknowledge notify interest with an empty data packet
+        logging.debug(f'acknowledging notify interest {Name.to_str(int_name)}')
+        self.app.put_data(int_name, None)
+
+    def _on_msg_interest(self, int_name, int_param, app_param):
+        aio.ensure_future(self._process_msg_interest(int_name, int_param, app_param))
+
+    async def _process_msg_interest(self, int_name, int_param, app_param):
+        """
+        Async helper for `_on_msg_interest()`.
+        """
+        logging.debug(f'received msg interest: {Name.to_str(int_name)}')
+        nonce = int(Component.to_str(int_name[-1]))
+
         # return data if corresponding nonce still exists
         if nonce in self.nonce_to_msg:
             self.app.put_data(int_name, self.nonce_to_msg[nonce])
-            logging.info(f'reply msg with name {Name.to_str(int_name)}')
+            logging.debug(f'reply msg with name {Name.to_str(int_name)}')
         else:
-            logging.info('no matching data found')
+            logging.debug(f'no matching data with nonce {nonce} found')
+
+    async def _erase_state_after(self, nonce: int, timeout: int):
+        """
+        Erase state assoicated with `nonce` after `timeout`.
+        """
+        await aio.sleep(timeout)
+        if nonce in self.nonce_to_msg:
+            del self.nonce_to_msg[nonce]
+            logging.debug(f'erased state for nonce {nonce}')
