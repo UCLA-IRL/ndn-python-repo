8a75d4ab3a5e6ff3dda460652dfda5e3eedaf486
Xinyu Ma <ma.xinyu.26a@kyoto-u.jp>
2022-05-26 00:30:34 -0700

    Fix broken tests (upgrade packages)

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ setup(
     name='ndn-python-repo',
     version=__version__,
     description='An NDN Repo implementation using Python',
-    url='https://github.com/JonnyKong/ndn-python-repo',
+    url='https://github.com/UCLA-IRL/ndn-python-repo',
     author='Zhaoning Kong',
     author_email='jonnykong@cs.ucla.edu',
     download_url='https://pypi.python.org/pypi/ndn-python-repo',
     project_urls={
-        "Bug Tracker": "https://github.com/JonnyKong/ndn-python-repo/issues",
-        "Source Code": "https://github.com/JonnyKong/ndn-python-repo",
+        "Bug Tracker": "https://github.com/UCLA-IRL/ndn-python-repo/issues",
+        "Source Code": "https://github.com/UCLA-IRL/ndn-python-repo",
     },
     license='Apache License 2.0',
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -28,9 +28,8 @@ setup(
 
         'License :: OSI Approved :: Apache Software License',
 
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 
     keywords='NDN',
@@ -38,15 +37,15 @@ setup(
     packages=find_packages(exclude=['tests']),
 
     install_requires=[
-        "python-ndn >= 0.2b2.post1",
-        "PyYAML >= 5.1.2",
+        "python-ndn >= 0.3.post1",
+        "PyYAML >= 6.0",
     ],
     extras_require={
         'test': [ 'pytest', 'pytest-cov'],
         'leveldb': ['plyvel'],
         'mongodb': ['pymongo']
     },
-    python_requires=">=3.7",
+    python_requires=">=3.9",
 
     entry_points={
         'console_scripts': [
